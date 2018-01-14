a76c372e3401750d4039b69fad992bb693b2f405
ngloba <nataliiagloba@gmail.com>
2018-01-14 20:31:49 +0200

    Added some functions

--- sandbox/src/main/java/nglo/training/MyFirstProgram.java
+++ sandbox/src/main/java/nglo/training/MyFirstProgram.java
@@ -3,7 +3,26 @@ package nglo.training;
 public class MyFirstProgram {
 
 public static void main (String[] args) {
-System.out.println("Hello world");
+    hello("Thomaas");
+
+    double len = 0.5;
+    System.out.println("square of "+ len +"= " +area(len));
+
+    double a = 15;
+    double b=25;
+
+    System.out.println("площадь прямоугольника со сторонами " +a+ " и " +b+ "= " +area(a,b));
 }
+   public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody);
+    }
+
+    public static double area (double l) {
+        return l*l;
+    }
+
+    public static double area (double a, double b) {
+    return a*b;
+    }
 
 }
